e353cc34d898f523afc29639b2840e29724d2b14
ileostar <hi@leostar.top>
2024-03-08 00:50:46 +0800

    chore: project init

---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+defineOptions({
+  name: 'IndexPage',
+})
+</script>
+
+<template>
+  <div>
+    <TheFooter />
+  </div>
+</template>
